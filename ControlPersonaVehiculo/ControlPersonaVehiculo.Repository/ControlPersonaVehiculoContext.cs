9d6f5b3246c4161cfe83d469c87d8389941c6fba
rpacci <rpaccipomacosi@hotmail.com>
2015-04-07 01:28:29 -0500

    commit 2

---
+++ ControlPersonaVehiculo/ControlPersonaVehiculo.Repository/ControlPersonaVehiculoContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlPersonaVehiculo.Repository
+{
+    public class ControlPersonaVehiculoContext : DbContext
+    {
+        public ControlPersonaVehiculoContext() : base("name = ControlPersonaVehiculo")
+        {
+ 
+        }
+    }
+}
