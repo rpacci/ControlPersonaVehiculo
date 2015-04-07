b15b3d5c03d19ef35d9f7e1eabf303254e26f2d6
rpacci <rpaccipomacosi@hotmail.com>
2015-04-07 02:01:37 -0500

    commit 2

--- ControlPersonaVehiculo/ControlPersonaVehiculo.Dominio/Persona.cs
+++ ControlPersonaVehiculo/ControlPersonaVehiculo.Dominio/Persona.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ControlPersonaVehiculo.Dominio
 {
-    public class Class1
+    public class Persona
     {
+        [Key]
+        public int IDPersona { get; set; }
     }
 }
--- ControlPersonaVehiculo/ControlPersonaVehiculo.Dominio/ZonaTrabajo.cs
+++ ControlPersonaVehiculo/ControlPersonaVehiculo.Dominio/ZonaTrabajo.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ControlPersonaVehiculo.Services
 {
-    public class Class1
+    public class ZonaTrabajo
     {
+        [Key]
+        public int IDZonaTrabajo { get; set; }
     }
 }
